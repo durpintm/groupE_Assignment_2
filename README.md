cb57799073f8706024a8b2db0c121f86a34483da
Mahie-kaur <157408894+Mahie-kaur@users.noreply.github.com>
2024-02-01 23:17:58 -0500

    Added Area of Rectangle (Veerpal Kaur, 5002264047)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# GroupE_Assignment_2" 
+Function Three : Calculate Area of Ractangle
+created by Veerpal Kaur(500226407)
+this function will Calculate the area of rectangle by entering length and width.
--- main.go
+++ main.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("This assignment belongs to Group E")
+	fmt.Println(" Calculate Area of Ractangle ")
+	fmt.Println(calculateareaofractangle())
+
+}
+
+// Function Three
+// created by Veerpal Kaur(500226407)
+// this function will Calculate the area of rectangle by entering length and width.
+func calculateareaofractangle() float64 {
+
+	var length, width float64
+
+	fmt.Println("Enter the length of the rectangle: ")
+	fmt.Scan(&length)
+	fmt.Println("Enter the Width of the rectangle: ")
+	fmt.Scan(&width)
+
+	return length * width
 }
